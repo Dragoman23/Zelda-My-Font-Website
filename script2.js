8e94e19f914f2b3fab3f4679cdd590ebd85e3a12
Ram <ramkondapalli23@gmail.com>
2026-07-08 03:43:24 -0400

    Königsberg map added and back to original button

---
+++ script2.js
@@ -0,0 +1,23 @@
+console.log("Script loaded.");
+const frequencyButton = document.getElementById('frequency-button');
+const wordFrequencyButton = document.getElementById("words-frequency-button");
+const oneLeterButton = document.getElementById("isolate-words-button");
+const threeFrequencyButton = document.getElementById("most-frequency-button");
+const vowelFrequencyButton = document.getElementById("vowel-frequency");
+const cipherButton = document.getElementById("cipher-hint-button");
+const mapButton = document.getElementById("map-hint-button");
+const backToOriginalButton = document.getElementById("back-to-original-button");
+
+const letter = document.createElement("letter");
+const word = document.createElement("word");
+const one = document.createElement("one");
+const three = document.createElement("three");
+const vowel = document.createElement("vowel");
+const cipher = document.createElement("cipher");
+const map = document.createElement("map");
+
+
+backToOriginalButton.addEventListener('click', function() {
+    const container = document.querySelector(".translate-text");
+    container.innerHTML = "";
+})
